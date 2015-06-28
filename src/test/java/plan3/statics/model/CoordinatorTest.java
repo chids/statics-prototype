c3557d77b717903b80ed77fb4c23e27cd3b36db6
Mårten Gustafson <marten.gustafson@gmail.com>
2015-06-28 20:49:40 +0200

    Bring CoordinatorTest in line with changes to add and update.

--- src/test/java/plan3/statics/model/CoordinatorTest.java
+++ src/test/java/plan3/statics/model/CoordinatorTest.java
@@ -1,6 +1,9 @@
 package plan3.statics.model;
 
+import static javax.ws.rs.core.HttpHeaders.ETAG;
+import static javax.ws.rs.core.HttpHeaders.LOCATION;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.fail;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
@@ -9,8 +12,8 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
-import plan3.pure.jersey.exceptions.PreconditionFailedException;
+import plan3.statics.exceptions.DoesntExistException;
+import plan3.statics.exceptions.InternalConflictException;
 import plan3.statics.mocks.MockCache;
 import plan3.statics.mocks.MockLock;
 import plan3.statics.mocks.MockStorage;
@@ -18,6 +21,8 @@ import plan3.statics.model.commands.AddCommand;
 
 import java.util.Observer;
 import java.util.concurrent.Callable;
+
+import javax.ws.rs.core.MultivaluedMap;
 
 import org.junit.Test;
 
@@ -46,14 +51,22 @@ public class CoordinatorTest {
         coordinator.cache.put(version2); // Write version 2 only to cache
         try {
             coordinator.add(version2.update("bloh")); // Attempt to write version 3
-            fail("Expected conflict");
+            fail("Exception expected");
         }
-        catch(final RevisionMismatchException expeted) {}
+        catch(final InternalConflictException expected) {
+            final MultivaluedMap<String, Object> headers = expected.getResponse().getMetadata();
+            assertEquals(version2.where().revision(), headers.getFirst(ETAG));
+            assertEquals(version2.where().toString('/'), headers.getFirst(LOCATION).toString());
+        }
         try {
             coordinator.add(version2.update("bloh")); // Attempt to write version 3, again
-            fail("Expected conflict");
+            fail("Exception expected");
         }
-        catch(final RevisionMismatchException expected) {}
+        catch(final InternalConflictException expected) {
+            final MultivaluedMap<String, Object> headers = expected.getResponse().getMetadata();
+            assertEquals(version2.where().revision(), headers.getFirst(ETAG));
+            assertEquals(version2.where().toString('/'), headers.getFirst(LOCATION).toString());
+        }
     }
 
     @Test
@@ -79,22 +92,36 @@ public class CoordinatorTest {
         verify(observer, times(1)).update(coordinator, version2.where());
     }
 
-    @Test(expected = RevisionMismatchException.class)
+    @Test
     public void overwriteWithAdd() throws Exception {
         final Coordinator coordinator = coordinator();
         final Content version1 = new Content("domain", "type", "id", "blah");
         coordinator.add(version1);
-        coordinator.add(version1.update("bleh"));
+        try {
+            coordinator.add(version1.update("bleh"));
+        }
+        catch(final InternalConflictException expected) {
+            final MultivaluedMap<String, Object> headers = expected.getResponse().getMetadata();
+            assertEquals(version1.where().revision(), headers.getFirst(ETAG));
+            assertEquals(version1.where().toString('/'), headers.getFirst(LOCATION).toString());
+        }
     }
 
-    @Test(expected = PreconditionFailedException.class)
+    @Test
     public void cacheDoesntMatchStorageConflict() throws Exception {
         final ObservableCoordinator coordinator = coordinator();
         final Content version1 = new Content("domain", "type", "id", "blah");
         final Location revision1 = coordinator.add(version1);
         final Content version2 = version1.update("foo");
         coordinator.cache.put(version2); // Write version 2 to cache only
-        coordinator.update(revision1, version1.update("version 3"));
+        try {
+            coordinator.update(revision1, version1.update("version 3"));
+        }
+        catch(final InternalConflictException expected) {
+            final MultivaluedMap<String, Object> headers = expected.getResponse().getMetadata();
+            assertEquals(version2.where().revision(), headers.getFirst(ETAG));
+            assertEquals(version2.where().toString('/'), headers.getFirst(LOCATION).toString());
+        }
     }
 
     @Test
@@ -107,27 +134,41 @@ public class CoordinatorTest {
         coordinator.update(revision1, version1.update("version 3"));
     }
 
-    @Test(expected = RevisionMismatchException.class)
+    @Test
     public void inStorageButEvictedFromCache() throws Exception {
         final ObservableCoordinator coordinator = coordinator();
         final Content version1 = new Content("domain", "type", "id", "blah");
         final Location revision1 = coordinator.add(version1);
         coordinator.cache.remove(version1.where()); // Remove from cache only
-        coordinator.update(revision1, version1.update("version 2"));
+        try {
+            coordinator.update(revision1, version1.update("version 2"));
+            fail("Exception expected");
+        }
+        catch(final InternalConflictException expected) {
+            final MultivaluedMap<String, Object> headers = expected.getResponse().getMetadata();
+            assertEquals(version1.where().revision(), headers.getFirst(ETAG));
+            assertEquals(version1.where().toString('/'), headers.getFirst(LOCATION).toString());
+        }
     }
 
     @Test
-    public void inCacheButRemovedFromStorageOverwrites() throws Exception {
+    public void inCacheButRemovedFromStorageRejects() throws Exception {
         final Observer observer = mock(Observer.class);
         final ObservableCoordinator coordinator = coordinator(observer);
         final Content version1 = new Content("domain", "type", "id", "blah");
         final Location revision1 = coordinator.add(version1);
         coordinator.storage.remove(version1.where()); // Remove from storage only
         final Content version2 = version1.update("version 2");
-        coordinator.update(revision1, version2);
-        assertEquals(coordinator.storage.get(version2.where()), version2);
+        try {
+            coordinator.update(revision1, version2);
+        }
+        catch(DoesntExistException expected) {
+            final MultivaluedMap<String, Object> headers = expected.getResponse().getMetadata();
+            assertEquals(version1.where().revision(), headers.getFirst(ETAG));
+            assertEquals(version1.where().toString('/'), headers.getFirst(LOCATION).toString());
+        }
+        assertNull(coordinator.storage.get(version2.where()));
         verify(observer, times(1)).update(coordinator, version1.where());
-        verify(observer, times(1)).update(coordinator, version2.where());
     }
 
     private static ObservableCoordinator coordinator(final Observer observer) {
